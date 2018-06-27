3c60937fc58f77d22f8acc8d58d9509a80d47f8d
dsasmita <dangs.work@gmail.com>
2018-06-27 17:03:22 +0700

    Add #pilkada2018 to navigation

--- resources/views/layouts/layout-master.blade.php
+++ resources/views/layouts/layout-master.blade.php
@@ -22,7 +22,7 @@
             <div class="navbar-header">
 
                 <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed hidden" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                     <span class="sr-only">Toggle Navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -38,6 +38,7 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
+                    <li><a href="#">#pilkada2018</a></li>
                 </ul>
 
                 <form class="navbar-form navbar-left hidden" role="search">
